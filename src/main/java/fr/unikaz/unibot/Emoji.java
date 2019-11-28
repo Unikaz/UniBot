5acd94231d1c719709d4fa35fbe4827670894024
Unikaz <unikaz@free.fr>
2019-11-28 23:41:56 +0100

    [CMD] Party - Use of Futures to limit calls impact - Remove general threading and lock to avoid being ban by Discord - Add Emoji.intToEmoji to print int with emoji

--- src/main/java/fr/unikaz/unibot/Emoji.java
+++ src/main/java/fr/unikaz/unibot/Emoji.java
@@ -5,19 +5,20 @@ import fr.unikaz.unibot.listeners.Party;
 import org.apache.commons.lang.ObjectUtils;
 
 public enum Emoji {
-	ZERO("\u0030\u20e3"),
-	ONE("\u0031\u20e3"),
-	TWO("\u0032\u20e3"),
-	THREE("\u0033\u20e3"),
-	FOUR("\u0034\u20e3"),
-	FIVE("\u0035\u20e3"),
-	SIX("\u0036\u20e3"),
-	SEVEN("\u0037\u20e3"),
-	EIGHT("\u0038\u20e3"),
-	NINE("\u0039\u20e3"),
+	ZERO("\u0030\u20e3", "0️⃣"),
+	ONE("\u0031\u20e3", "1️⃣"),
+	TWO("\u0032\u20e3", "2️⃣"),
+	THREE("\u0033\u20e3", "3️⃣"),
+	FOUR("\u0034\u20e3", "4️⃣"),
+	FIVE("\u0035\u20e3", "5️⃣"),
+	SIX("\u0036\u20e3", "6️⃣"),
+	SEVEN("\u0037\u20e3", "7️⃣"),
+	EIGHT("\u0038\u20e3", "8️⃣"),
+	NINE("\u0039\u20e3", "9️⃣"),
 	WHITE_CHECK_MARK("✅"),
 	X("❌");
 	public final String val;
+	public final String val2;
 
 	public static final Emoji[] DIGITS={
 		ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE
@@ -25,6 +26,11 @@ public enum Emoji {
 
 	Emoji(String val) {
 		this.val = val;
+		this.val2 = null;
+	}
+	Emoji(String val, String val2) {
+		this.val = val;
+		this.val2 = val2;
 	}
 
 	public static Emoji get(String val) {
@@ -40,5 +46,20 @@ public enum Emoji {
 				return i;
 		}
 		return -1;
+	}
+
+	public boolean is(String e) {
+		if(e == null) return false;
+		return e.equals(val) || e.equals(val2);
+	}
+
+	public static String intToEmoji(int value){
+		if(value == 0 ) return ZERO.val;
+		StringBuilder s = new StringBuilder();
+		while(value > 0){
+			s.insert(0, DIGITS[value % 10].val);
+			value /= 10;
+		}
+		return s.toString();
 	}
 }
