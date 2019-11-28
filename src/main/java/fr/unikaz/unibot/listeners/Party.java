726fa4798ecda0b775cf37897668e6ed04372877
Unikaz <unikaz@free.fr>
2019-11-28 23:41:56 +0100

    [CMD] Party - Use of Futures to limit calls impact - Remove general threading and lock to avoid being ban by Discord - Add Emoji.intToEmoji to print int with emoji

--- src/main/java/fr/unikaz/unibot/listeners/Party.java
+++ src/main/java/fr/unikaz/unibot/listeners/Party.java
@@ -9,7 +9,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.WeakHashMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -28,8 +31,6 @@ import net.dv8tion.jda.api.requests.restaction.pagination.ReactionPaginationActi
 
 public class Party {
 	public static final String KEY = "/party ";
-
-	public static final WeakHashMap<String, Object> LOCKS = new WeakHashMap<>();
 
 	@SubscribeEvent
 	public void onMessage(MessageReceivedEvent event) {
@@ -66,76 +67,87 @@ public class Party {
 
 	private void handleReactionEvent(GenericMessageReactionEvent event) {
 		if (event.getReaction().isSelf()) return;
-		String hash = event.getGuild().getId() + '-' + event.getChannel().getId() + '-' + event.getMessageId();
-		// Lock to avoid errors
-		Object lock = LOCKS.computeIfAbsent(hash, k -> new Object());
-		synchronized (lock) {
-			// We try to get the message from the local history to avoid calling the API everytime
-			Message message = event.getChannel().getHistory().getMessageById(event.getMessageId());
-			if (message == null) { // In the worst case, we call the API ^^'
-				message = event.getChannel().retrieveMessageById(event.getMessageId()).complete();
-			}
-			if (!Main.getJda().getSelfUser().equals(message.getAuthor())) return; // if the bot is not the author
-			MessageReaction reaction = event.getReaction();
-			if (!reaction.getReactionEmote().isEmoji()) return;
-			String e = reaction.getReactionEmote().getEmoji();
-			if (!Emoji.WHITE_CHECK_MARK.val.equals(e) &&
-				!Emoji.X.val.equals(e) &&
-				Arrays.stream(Emoji.DIGITS).noneMatch(emoji -> emoji.val.equals(e))) {
-				// random emoji... don't care
-				return;
-			}
-			Message finalMessage = message;
-			new Thread(() -> update(finalMessage)).start();
+		// We try to get the message from the local history to avoid calling the API everytime
+		Message message = event.getChannel().getHistory().getMessageById(event.getMessageId());
+		if (message == null) { // In the worst case, we call the API ^^'
+			message = event.getChannel().retrieveMessageById(event.getMessageId()).complete();
+		}
+		if (!Main.getJda().getSelfUser().equals(message.getAuthor())) return; // if the bot is not the author
+		MessageReaction reaction = event.getReaction();
+		if (!reaction.getReactionEmote().isEmoji()) return;
+		String e = reaction.getReactionEmote().getEmoji();
+		if (!Emoji.WHITE_CHECK_MARK.is(e) &&
+			!Emoji.X.is(e) &&
+			Arrays.stream(Emoji.DIGITS).noneMatch(emoji -> emoji.is(e))) {
+			// random emoji... don't care
+			return;
+		}
+
+		try {
+			update(message);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+			System.out.println("timeout... temp ban ?");
 		}
 	}
 
-	private void update(Message message) {
-		int total = 0; // to remove bot reactions
+	private void update(Message message) throws InterruptedException {
+		AtomicInteger total = new AtomicInteger();
+		List<Callable<Object>> callables = new ArrayList<>();
 		Map<String, Integer> yesMentions = new HashMap<>();
 		List<String> noMentions = new ArrayList<>();
-		int anonymous = 0;
+		AtomicInteger anonymous = new AtomicInteger();
 		for (MessageReaction reaction : message.getReactions()) {
 			if (!reaction.getReactionEmote().isEmoji()) continue;
-			if (reaction.getCount() == 1) continue; // dodge useless call
 			String e = reaction.getReactionEmote().getEmoji();
-			if (Emoji.WHITE_CHECK_MARK.val.equals(e)) {
-				total += reaction.getCount() - 1;
-				yesMentions = reaction.retrieveUsers().stream()
+			if (Emoji.WHITE_CHECK_MARK.is(e)) {
+				if (reaction.getCount() == 1) continue; // dodge useless call
+				total.addAndGet(reaction.getCount() - 1);
+				yesMentions.putAll(reaction.retrieveUsers().stream()
 					.filter(u -> !u.isBot())
-					.collect(Collectors.toMap(IMentionable::getAsMention, u -> 0));
-			} else if (Emoji.X.val.equals(e)) {
-				noMentions = reaction.retrieveUsers().stream()
-					.filter(u -> !u.isBot())
-					.map(IMentionable::getAsMention).collect(Collectors.toList());
+					.collect(Collectors.toMap(IMentionable::getAsMention, u -> 0)));
+			} else if (Emoji.X.is(e)) {
+				if (reaction.getCount() == 1) continue; // dodge useless call
+				callables.add(() -> {
+					noMentions.addAll(reaction.retrieveUsers().stream()
+						.filter(u -> !u.isBot())
+						.map(IMentionable::getAsMention).collect(Collectors.toList()));
+					return null;
+				});
 			} else {
 				for (int i = 0; i < Emoji.DIGITS.length; i++) {
-					if (Emoji.DIGITS[i].val.equals(e)) {
-						total += i * (reaction.getCount() - 1);
-						ReactionPaginationAction users = reaction.retrieveUsers();
-						for (User user : users) {
-							if (user.isBot()) continue;
-							if (yesMentions.containsKey(user.getAsMention())) {
-								yesMentions.put(user.getAsMention(), yesMentions.get(user.getAsMention()) + i);
-							} else {
-								anonymous += i;
+					if (Emoji.DIGITS[i].is(e)) {
+						int finalI = i;
+						callables.add(() -> {
+							ReactionPaginationAction users = reaction.retrieveUsers();
+							for (User user : users) {
+								if (user.isBot()) continue;
+								total.addAndGet(finalI);
+								if (yesMentions.containsKey(user.getAsMention())) {
+									yesMentions.put(user.getAsMention(), yesMentions.get(user.getAsMention()) + finalI);
+								} else {
+									anonymous.addAndGet(finalI);
+								}
 							}
-						}
+							return null;
+						});
 					}
 				}
 			}
 		}
+		// launch all call to API
+		ForkJoinPool.commonPool().invokeAll(callables, 10, TimeUnit.SECONDS);
 		String yes = "";
 		if (!yesMentions.isEmpty())
 			yes = yesMentions.entrySet().stream()
 				.map(e -> e.getKey() + (e.getValue() > 0 ? "(+" + e.getValue() + ")" : ""))
 				.collect(Collectors.joining(", "));
-		if (anonymous > 0)
+		if (anonymous.get() > 0)
 			yes += " +" + anonymous;
 		MessageEmbed embed = rebuildEmbed(message.getEmbeds().get(0).getTitle(),
 			yes,
 			!noMentions.isEmpty() ? String.join(", ", noMentions) : " ",
-			total);
+			total.get());
 		message.getChannel().editMessageById(message.getId(), embed).complete();
 	}
 
@@ -147,7 +159,7 @@ public class Party {
 			eb.setColor(Main.getConf().pollColor);
 		eb.addField(Emoji.WHITE_CHECK_MARK.val + ' ' + Main.getConf().partyTextYes, yesStr, false);
 		eb.addField(Emoji.X.val + ' ' + Main.getConf().partyTextNo, noStr, false);
-		eb.addField(Main.getConf().partyTextCounter, total + "", false);
+		eb.addField(Main.getConf().partyTextCounter, Emoji.intToEmoji(total), false);
 		eb.addField("", Main.getConf().partyText, false);
 		return eb.build();
 	}
